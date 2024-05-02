22f1e1d959fe46692fcecaf09c8b4a0e73d88c3e
Esther Csoke <esther.e.csoke@gmail.com>
2024-05-02 13:11:14 +1000

    fixed up file for draft of question 1

--- Q1Cleaning.R
+++ Q1Cleaning.R
@@ -13,15 +13,7 @@ library(psych)
 
 
 # read TMDB csv file
-tmdb <- read_csv("TMDB_movie_dataset_v11.csv")
-
-# select columns for Q1
-tmdb_q1 <- tmdb %>%
-  select(title, vote_average, release_date, revenue, status, genres, vote_count)
-
-
-# set release_date as date type
-tmdb_q1$release_date <- as.Date(tmdb_q1$release_date, format="%Y-%m-%d")
+tmdb_q1 <- read_csv("tmdb.csv")
 
 
 # filter by stats = released
@@ -30,56 +22,38 @@ tmdb_q1$release_date <- as.Date(tmdb_q1$release_date, format="%Y-%m-%d")
 # genres
 # https://dplyr.tidyverse.org/reference/filter.html
 
-tmdb_q1 <- tmdb_q1 %>%
-  filter(status == "Released", ignore.case = TRUE, na.rm = TRUE) %>%
-  filter(revenue != '0', na.rm = TRUE ) %>%
-  drop_na(release_date) %>%
-  drop_na(genres) %>%
-  filter(vote_count > 1000) 
 
-
-# Convert to millions and rounding to two decimal places
-tmdb_q1$revenue <- round(tmdb_q1$revenue / 1e6, 2)
-
-
-# sep columns for genres
-tmdb_q1 <- tmdb_q1 %>% separate(genres, c("genre1"), ",")
-
-
-# check distribution
-# first look showed a lot of 0s for revenue and vote_average
-movies <- tmdb_q1 %>% 
-  mutate(year = as.integer(substr(release_date, 1, 4)))
-
-
-# check how many duplicates are in the 
-duplicate_counts <- movies %>%
-  group_by(title, year) %>%
-  summarize(count = n(), .groups = 'drop') %>%
-  filter(count > 1) %>%
-  select(title, count)
 
 
 # year year annd month
-tmdb_q1$year <- year(tmdb_q1$release_date)
 tmdb_q1$month <- month(tmdb_q1$release_date)
 
 # group by year and month
 # average the rating by film genre
-
 # Group and summarising
-final_q1 <- tmdb_q1 %>%
-  group_by(year, month, genre1) %>%
-  summarise(average_rating = mean(vote_average), .groups = 'drop')
+tmdb_q1 <- tmdb_q1 %>%
+  group_by(release_year, month, genre) %>%
+  summarise(
+    average_rating = mean(vote_average, na.rm = TRUE),  
+    average_revenue = mean(revenue, na.rm = TRUE),      
+    .groups = 'drop'  
+  )
 
-
-# convert months to char (names)
-final_q1$month <- month.name[final_q1$month]
 
 
 # round  average rating to 2 dec point
-final_q1$average_rating <- round(final_q1$average_rating, 2)
+tmdb_q1$average_rating <- round(tmdb_q1$average_rating, 2)
 
+tmdb_q1$average_revenue <- round(tmdb_q1$average_revenue, 2)
+
+
+# Check for NA values in year or month
+final_q1 <- tmdb_q1 %>%
+  filter(!is.na(year) & !is.na(month) & !is.na(average_revenue))
+
+
+# Convert year and month to a date (first day of each month)
+tmdb_q1$date_ts <- make_date(final_q1$release_year, final_q1$month, 1)
 
 
 # descriptive stats
@@ -88,7 +62,11 @@ str(tmdb_q1)
 
 plot_histogram(tmdb_q1)
 
-describe(tmdb_q1)
+plot_intro(tmdb_q1)
+
+
+# write to csv
+write_csv(tmdb_q1,"question1.csv")
 
 
 
